c73db51751c53b0e22d51792b48256c6238fcdec
Matheus Henrique Gonzaga <matheushgrdc156@gmail.com>
2023-11-14 22:57:50 -0300

    Ajusta template do index e script

--- scripts/generate_pages.py
+++ scripts/generate_pages.py
@@ -1,6 +1,10 @@
 import json
 from jinja2 import Environment, FileSystemLoader
 
+def remove_special_characters(text):
+    # Remove apenas os acentos, mantendo as letras com acento
+    return text.replace(' ', '_').replace('á', 'a').replace('ã', 'a').replace('é', 'e').replace('í', 'i').replace('ó', 'o').replace('ú', 'u').replace('ç', 'c').lower()
+
 def generate_pages(posts_data):
     env = Environment(loader=FileSystemLoader('templates'))
     template = env.get_template('post_template.html')
@@ -10,28 +14,31 @@ def generate_pages(posts_data):
         description = post['description']
         author = post['author']
         date = post['date']
-        image = post.get('image', '')  # Pode não haver uma imagem em todos os posts
+        image = post.get('image', '')
+
+        # Use a função para remover acentos no nome do arquivo
+        file_name = remove_special_characters(title)
 
         html_content = template.render(title=title, description=description, author=author, date=date, image=image)
 
-        with open(f'docs/{title.lower().replace(" ", "_")}.html', 'w') as file:
+        with open(f'docs/{file_name}.html', 'w') as file:
             file.write(html_content)
 
 def generate_index_page(posts_data):
-    """
-    Gera o arquivo index.html na pasta docs.
-    Inclui links para a documentação e as postagens geradas.
-    """
     index_template_path = 'templates/index_template.html'
     docs_output_path = 'docs/index.html'
 
     with open(index_template_path, 'r') as template_file:
         template_content = template_file.read()
 
-    # Adicione links para a documentação e postagens geradas
-    # Use os caminhos relativos corretos para a documentação e postagens
+    # Crie os links com remoção de acentos e minúsculas
+    links = '\n'.join([
+        f'<li><a href="{remove_special_characters(post["title"])}.html">{remove_special_characters(post["title"])}</a></li>' for post in posts_data
+    ])
+    
     context = {
-        'posts': posts_data
+        'posts': posts_data,
+        'links': links
     }
     updated_content = Environment(loader=FileSystemLoader('templates')).from_string(template_content).render(context)
 
