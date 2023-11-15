04c792a661fe97dc1338d5de4905925f4be04115
Matheus Henrique Gonzaga <matheushgrdc156@gmail.com>
2023-11-14 22:15:10 -0300

    Adiciona um arquivo de documentação

--- scripts/generate_pages.py
+++ scripts/generate_pages.py
@@ -17,8 +17,30 @@ def generate_pages(posts_data):
         with open(f'docs/{title.lower().replace(" ", "_")}.html', 'w') as file:
             file.write(html_content)
 
+def generate_index_page(posts_data):
+    """
+    Gera o arquivo index.html na pasta docs.
+    Inclui links para a documentação e as postagens geradas.
+    """
+    index_template_path = 'templates/index_template.html'
+    docs_output_path = 'docs/index.html'
+
+    with open(index_template_path, 'r') as template_file:
+        template_content = template_file.read()
+
+    # Adicione links para a documentação e postagens geradas
+    # Use os caminhos relativos corretos para a documentação e postagens
+    context = {
+        'posts': posts_data
+    }
+    updated_content = Environment(loader=FileSystemLoader('templates')).from_string(template_content).render(context)
+
+    with open(docs_output_path, 'w') as output_file:
+        output_file.write(updated_content)
+
 if __name__ == "__main__":
     with open('posts/posts.json', 'r') as file:
         posts_data = json.load(file)
 
     generate_pages(posts_data)
+    generate_index_page(posts_data)
